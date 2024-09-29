240e38f370a6969ca9a0854502ab9237013a3bce
Samad Khan <sam.sal3304@gmail.com>
2024-09-29 20:27:30 +0500

    First Commit Ever

---
+++ first.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello, World!";
+}
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
